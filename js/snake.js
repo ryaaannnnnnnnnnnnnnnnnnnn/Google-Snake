db9b23681ceeba8d7b258b6347520070b6a05db1
Ryan <126rgranada@frhsd.com>
2025-03-03 14:02:25 -0500

    Day 4

--- js/snake.js
+++ js/snake.js
@@ -1,22 +1,17 @@
+import { getInputDirection } from " ./input.js";
+
 export const SNAKE_SPEED = 2;
-const snakeBody = [
-     { x:11, y: 11},
-     { x: 11, y: 11},
-     { x: 12, y: 11},
-     { x: 13, y: 11},
-     { x: 14, y: 11},
-     { x: 15, y: 11}
-
-
-]
+const snakeBody = [{ x:11, y: 11}]
 
 export function update(){
+   const inputDirection = getInputDirection();  
+     
      for(let i = snakeBody.length - 2; i >= 0; i--) {
      snakeBody[i + 1] = { ...snakeBody[i] };
     }
 
-    snakeBody[0].x += 1;
-    snakeBody[0].y += 0;
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 
 }
 
@@ -29,4 +24,19 @@ export function draw(gameBoard){
     snakeElement.classList.add('snake');
     gameBoard.appendChild(snakeElement);
    })
+}
+
+export function expandSnake(amount){
+     newSegments += amount;
+}
+
+export function onSnake(position) {
+     return snakeBody.some(segment => {
+          return equalPositions(segment, position);
+     })
+
+}
+
+function equalPositions(pos1, pos2) {
+     return pos1.x === pos2.x && pos1.y === pos2.y;
 }
